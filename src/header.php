f544a7fd0f0de7832692f9a2104d1e842c85f9ec
kana <2015edcckanaitai@gmail.com>
2024-12-14 19:53:34 +0900

    header.phpを修正。 googleアナリティクスのタグを追加。

--- src/header.php
+++ src/header.php
@@ -4,7 +4,19 @@
   <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php wp_head() ?>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-QFFQ32NN25"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-QFFQ32NN25');
+    </script>
+    <?php wp_head(); ?>
   </head>
 
   <body class="js-body">
